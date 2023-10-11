4aa03f8de804a53f316a8c0b34fd7b8577beb4da
Diego Veiga <diegogv.eng@gmail.com>
2023-10-10 23:51:08 -0300

    Reset com clear do S3

--- backend/base_app/utils/reset_environment_data.py
+++ backend/base_app/utils/reset_environment_data.py
@@ -1,12 +1,16 @@
+from django.conf import settings
 from django.core.management import call_command
+from project.core_models.aws_config import AWSConfig
 from base_app.scripts import create_dummy_users
 
 
 def reset_environment_data():
     print('Running reset_environment_data...')
+    aws_config: AWSConfig = settings.ROOT_OBJECT.aws_config
 
     # flushing and inserting data
     call_command('flush', '--no-input', verbosity=1)
+    aws_config.clear_bucket()
 
     # creating dummy users
     create_dummy_users()
