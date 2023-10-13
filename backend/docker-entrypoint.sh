971002c011ba63c970b2265e3e940327d840b1b6
Diego Veiga <diegogv.eng@gmail.com>
2023-10-13 16:52:51 -0300

    Add collectstatic no entrypoint

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -17,6 +17,7 @@ python manage.py migrate
 echo "flushing data and loading fistures..."
 python manage.py flush --noinput
 python manage.py shell < base_app/utils/reset_environment_data.py
+python manage.py collectstatic --no-input
 
 # Start backend
 python manage.py runserver 0.0.0.0:8000
