03452d237edc6c464241a32b6207a859cdfcdf5d
Diego Veiga <diegogv.eng@gmail.com>
2023-10-13 20:55:36 -0300

    WIP: Inserindo dados de media para demo

--- backend/base_app/utils/reset_environment_data.py
+++ backend/base_app/utils/reset_environment_data.py
@@ -1,12 +1,21 @@
+import os
 from django.conf import settings
 from django.core.management import call_command
 from project.core_models.aws_config import AWSConfig
 
 
+def _files_to_upload_to_data_folder():
+    path = os.path.join(settings.PROJECT_DIR, 'project/media/')
+    files = []
+    for (dirpath, dirnames, filenames) in os.walk(path):
+        files.extend(filenames)
+        break
+    return (path, files)
+
 def load_demo_fixture(apps, schema_editor, verbosity=2):
     print('Carregando dados de PlotStep...')
     call_command(
-        'loaddata', 'backend/base_app/fixtures/defaults/dumps/demo.json', verbosity=verbosity)
+        'loaddata', 'base_app/fixtures/defaults/dumps/demo.json', verbosity=verbosity)
 
 def reset_environment_data():
     print('Running reset_environment_data...')
@@ -16,6 +25,9 @@ def reset_environment_data():
     call_command('flush', '--no-input', verbosity=1)
     aws_config.clear_bucket()
 
+    path, filenames = _files_to_upload_to_data_folder()
+    aws_config.insert_files_in_bucket_directory(path, filenames)
+    
     # loading demostration fixture
     load_demo_fixture(None, None)
     
